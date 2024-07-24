f9fff824ef3a56b390f633b4e83fd440e0a91281
shenmengye <shenmengye@qiniu.com>
2024-07-24 17:14:16 +0800

    Added AI type constants and translation record structures.

--- internal/common/common.go
+++ internal/common/common.go
@@ -34,3 +34,39 @@ func IsValidURL(u string) (*url.URL, bool) {
 	}
 	return parsedURL, true
 }
+
+type AiType string
+
+const (
+	OpenAI   AiType = "openai"
+	DeepSeek AiType = "deepseek"
+	MoonShot AiType = "moonshot"
+	Groq     AiType = "groq"
+)
+
+var AiTypes = []AiType{OpenAI, DeepSeek, MoonShot, Groq}
+
+type OneAiRecord struct {
+	Cost        int64 `json:"cost"` // 耗时
+	InputToken  int   `json:"input_token"`
+	OutputToken int   `json:"output_token"`
+}
+
+type TranslateStepRecord struct {
+	StepName string         `json:"sname"`    //本次Step的名字
+	SumCost  int64          `json:"sum_cost"` //本次Step的耗时
+	Records  []*OneAiRecord `json:"records"`
+}
+
+type TranslateRecord struct {
+	SourceLang  string                 `json:"source_lang"`
+	TargetLang  string                 `json:"target_lang"`
+	MaxTokens   int                    `json:"max_token"`    // 最大token
+	InputTokens int                    `json:"input_tokens"` // 输入token个数
+	ChunksCount int                    `json:"chunks_count"` // 分块个数
+	Steps       []*TranslateStepRecord `json:"steps"`
+}
+
+func (t *TranslateRecord) String() string {
+
+}
