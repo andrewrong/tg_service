8411cdac4e39f729cf06e7588bd580d1afc64c65
shenmengye <shenmengye@qiniu.com>
2024-07-27 14:36:29 +0800

    Added OggToMp3 conversion function to audio.go.

--- internal/common/audio.go
+++ internal/common/audio.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/faiface/beep/vorbis"
+	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/wav"
 
 	"tg_ai_service/internal/log"
@@ -41,5 +42,40 @@ func OggToWav(reader io.ReadCloser, writer io.WriteSeeker) error {
 			Code:    0,
 		}
 	}
+
+func OggToMp3(reader io.ReadCloser, writer io.WriteSeeker) error {
+	if reader == nil || writer == nil {
+		log.Errorf("invalid reader or writer")
+		return &InnerError{
+			ErrType: ParameterError,
+			ErrMsg:  "invalid reader or writer",
+			Code:    0,
+		}
+	}
+
+	streamer, format, err := vorbis.Decode(reader)
+	if err != nil {
+		log.Errorf("decode ogg error: %v", err)
+		return &InnerError{
+			ErrType: ExternalServiceError,
+			ErrMsg:  fmt.Sprintf("decode ogg error:%s", err.Error()),
+			Code:    0,
+		}
+	}
+	defer streamer.Close()
+
+	// 将音频数据从OGG流写入MP3文件
+	err = mp3.Encode(writer, streamer, format)
+	if err != nil {
+		log.Errorf("encode mp3 error: %v", err)
+		return &InnerError{
+			ErrType: ExternalServiceError,
+			ErrMsg:  fmt.Sprintf("encode mp3 error:%s", err.Error()),
+			Code:    0,
+		}
+	}
+	return nil
+}
+
 	return nil
 }
